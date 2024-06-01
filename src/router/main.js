b2945f9f119d1d65f7a574f6e48249e24c64db8a
wohu <hugo2004esperanza@gmail.com>
2024-06-01 22:09:19 +0200

    Código comentado

--- src/router/main.js
+++ src/router/main.js
@@ -70,51 +70,42 @@ router.get('/blog', async (req, res) => {
     }
 
     res.render('blog_posts', { blog_posts: rows, user_regis, favorites });
-
-    // try {
-        
-    // } catch (error) {
-    //     console.error('Error fetching blog posts', error);
-    //     res.status(500).send('Error fetching blog posts');
-    // } finally {
-    //     if (connection) {
-    //         await connection.end();
-    //     }
-    // }
 });
 
 
 
 // Ruta para agregar un post a favoritos
 router.post('/favorite', async (req, res) => {
+    //Si el usuario no está registrado, se le envía al login
     if (!user_regis) {
         res.redirect('/login');
         return;
     }
 
+    //Coge el ID del post
     const { postId } = req.body;
     const connection = await connectBD();
 
-    // Obtener el user_id del usuario actual
+    // Desde la variable del usuario actual, se coge el id de usuario
     const [userData] = await connection.execute('SELECT id FROM skate_users WHERE username = ?', [currentUser]);
     const userId = userData[0].id;
 
-    // Verificar si el post ya está en la lista de favoritos del usuario
+    // Verifica si el post ya está en la lista de favoritos del usuario registrado
     const [existingFavorite] = await connection.execute('SELECT * FROM user_favorites WHERE user_id = ? AND post_id = ?', [userId, postId]);
     if (existingFavorite.length > 0) {
-        // El post ya está en la lista de favoritos, no es necesario añadirlo de nuevo
+        // Si ya está en la lista de favoritos, no se añade de nuevo
         res.redirect('/blog');
         return;
     }
 
-    // Insertar el post favorito en la base de datos
+    // Inserta el post favorito en la base de datos
     await connection.execute('INSERT INTO user_favorites (user_id, post_id) VALUES (?, ?)', [userId, postId]);
 
     res.redirect('/blog');
 });
 
 
-// Ruta para eliminar un post favorito
+// Ruta para eliminar un post de favoritos
 router.post('/favorite/delete/:postId', async (req, res) => {
     if (!user_regis) {
         res.redirect('/login');
@@ -124,110 +115,119 @@ router.post('/favorite/delete/:postId', async (req, res) => {
     const { postId } = req.params;
     const connection = await connectBD();
 
-    // Obtener el user_id del usuario actual
     const [userData] = await connection.execute('SELECT id FROM skate_users WHERE username = ?', [currentUser]);
     const userId = userData[0].id;
 
-    // Eliminar el post favorito de la base de datos
+    // Elimina el post favorito de la base de datos y del perfil de usuario
     await connection.execute('DELETE FROM user_favorites WHERE user_id = ? AND post_id = ?', [userId, postId]);
 
     res.redirect('/cuenta');
 });
 
 
+//Ruta para el post que se ha hecho click en el blog
 router.get('/post/:nombre', async(req, res) => {
 
     const {nombre} = req.params
     const connection = await connectBD()
+    //Coge los campos del post de la base de datos
     const [rows, fields] = await connection.execute("SELECT * FROM blog_posts WHERE nombre = ?", [nombre])
-    if (!rows || rows.length === 0) {
-        console.error('No se encontraron publicaciones con el nombre proporcionado');
-        // Manejar el caso en el que no se encuentran publicaciones con el nombre proporcionado
-        return res.status(404).send('No se encontraron publicaciones con el nombre proporcionado');
-    }
+
     const post = rows[0]
 
+    //Coge los comentarios del post de la base de datos
     const [comments] = await connection.execute("SELECT * FROM blog_comments WHERE post_id = ?", [post.id])
 
     res.render('post', {post, comments, user_regis, currentUser})
 
 })
 
+//Ruta para añadir un comentario del usuario registrado
 router.post('/post/:nombre/comment', async(req, res) => {
     if (!user_regis) {
         res.redirect('/login');
         return;
     }
 
+    //Se coge el nombre del post
     const { nombre } = req.params;
+    //Coge la información que se ha puesto en el comentario
     const { comentario } = req.body;
     const connection = await connectBD();
 
-    // Obtener la información del post
     const [postInfo] = await connection.execute("SELECT id FROM blog_posts WHERE nombre = ?", [nombre]);
     const postId = postInfo[0].id;
 
-    // Obtener el user_id del usuario actual
     const [userData] = await connection.execute('SELECT id FROM skate_users WHERE username = ?', [currentUser]);
     const userId = userData[0].id;
 
-    // Insertar el comentario en la base de datos
+    // Insertar el comentario en la base de datos y en el post
     await connection.execute('INSERT INTO blog_comments(post_id, user_id, username, comentario) VALUES (?,?,?,?)', [postId, userId, currentUser, comentario]);
 
+    //Redirecciona al post al que se ha comentado
     res.redirect(`/post/${nombre}`);
 });
 
 
-
+//Ruta para eliminar un comentario del usuario registrado
 router.post('/post/:nombre/comment/:commentId/delete', async(req, res) => {
+    //Se coge el nombre del post y el id del comentario que queremos borrar
     const { nombre, commentId } = req.params;
     const connection = await connectBD();
 
     // Verifica si el comentario pertenece al usuario registrado
     const [comment] = await connection.execute('SELECT * FROM blog_comments WHERE id = ?', [commentId]);
     if (comment.length > 0 && comment[0].username === currentUser) {
-        // Elimina el comentario si el usuario registrado es el propietario del comentario
+        //Si es así, lo borra de la base de datos y del post
         await connection.execute('DELETE FROM blog_comments WHERE id = ?', [commentId]);
     }
 
     res.redirect(`/post/${nombre}`);
 });
 
+//Ruta para ir a la ayuda
 router.get('/ayuda', (req, res) => {
 
     res.render('contacto')
 
 })
 
+//Ruta para registrarse
 router.get('/login', (req, res) => {
 
     res.render('login')
 
 })
 
+//Ruta para registrarse con el usuario introducido
 router.post('/login', async(req, res) => {
 
+    //Coge el nombre de usuario y la contraseña que se ha introducido
     const {username, pass} = req.body
 
     const connection = await connectBD()
 
+    //Comprueba que el usuario exista
     const [rows, fields] = await connection.execute('SELECT * FROM skate_users WHERE username = ?', [username])
 
     if(rows.length > 0) {
 
         const user = rows[0]
+        //Desencripta la contraseña de la base de datos y comprueba si es igual a la que se ha introducido
         const passMatch = await bcrypt.compare(pass, user.pass)
 
+        //Si la contraseña coincide
         if(passMatch) {
 
-            user_regis = true
-            currentUser = user.username
-            currentEmail = user.email
-            res.redirect('/')
+            user_regis = true //Se pone a true la variable de registro
+            currentUser = user.username //El usuario actual es el usuario registrado
+            currentEmail = user.email //El correo actual es el del usuario registrado
+            res.redirect('/') //Se redirecciona a inicio
 
         }
         else{
 
+            //Si la contraseña no coincide, salta un error 400
             res.status(400).render('datosIncorrectos')
 
         }
@@ -235,33 +235,37 @@ router.post('/login', async(req, res) => {
     }
     else {
 
+        //Si algún otro parámetro introducido no es correcto, salta un error 400
         res.status(400).render('datosIncorrectos')
 
     }
 
 })
 
+//Ruta para registrarse
 router.get('/register', (req, res) => {
 
     res.render('register')
 
 })
 
+//Ruta para registrar un nuevo usuario
 router.post('/register', async(req, res) => {
-    const { username, email, pass, confirmPass } = req.body;
-    const encriptPass = await bcrypt.hash(pass, 10);
+    const { username, email, pass, confirmPass } = req.body; //Coge los campos introducidos en el formulario
+    const encriptPass = await bcrypt.hash(pass, 10); //Encripta la contraseña en la base de datos
     const connection = await connectBD();
 
+    //Si las contraseñas no coinciden, salta un error 400
     if(pass != confirmPass) {
 
         return res.status(400).render('contraseñaIncorrecta')
 
     }
 
-    // Verificar si el correo electrónico o el nombre de usuario ya están registrados
+    // Verifica si el correo electrónico o el nombre de usuario ya están registrados
     const [existingUser] = await connection.execute('SELECT * FROM skate_users WHERE email = ? OR username = ?', [email, username]);
     if (existingUser.length > 0) {
-        // Si el correo electrónico o el nombre de usuario ya están en uso, enviar una respuesta de error correspondiente
+        // Si el correo o el usuario ya está registrado, salta un error 400
         if (existingUser[0].email === email) {
             return res.status(400).render('emailRegistrado');
         } else {
@@ -269,8 +273,10 @@ router.post('/register', async(req, res) => {
         }
     }
 
+    //Inserta un nuevo usuario en la base de datos
     await connection.execute('INSERT INTO skate_users (username, email, pass) VALUES (?,?,?)', [username, email, encriptPass]);
 
+    //Opciones del email de confirmación de registro
     const mailOptions = {
         from: 'contactskatespoint@gmail.com',
         to: email,
@@ -280,6 +286,7 @@ No es necesario que respondas a este correo
 Disfruta de nuestro contenido ahora: http://localhost:3000/`
     };
 
+    //Comprobación de errores
     transporter.sendMail(mailOptions, function(error, info) {
         if (error) {
             console.log(error);
@@ -292,13 +299,16 @@ Disfruta de nuestro contenido ahora: http://localhost:3000/`
 });
 
 
+//Ruta para acceder a la cuenta
 router.get('/cuenta', (req, res) => {
 
+    //Si el usuario está registrado, se va al perfil
     if(user_regis) {
 
         res.redirect('/cuenta/'+currentUser)
 
     }
+    //Si no, se le lleva al login
     else {
 
         res.redirect('/login')
@@ -307,38 +317,44 @@ router.get('/cuenta', (req, res) => {
 
 })
 
+//Ruta para acceder a la cuenta del usuario
 router.get('/cuenta/:username', async (req, res) => {
+    //Si el usuario registrado y el nombre de usuario que se ha pedido en la url son el mismo, se lleva al perfil
     if (user_regis && req.params.username === currentUser) {
         const connection = await connectBD();
 
-        const [favorites] = await connection.execute(`
-            SELECT bp.* FROM blog_posts bp
-            JOIN user_favorites uf ON bp.id = uf.post_id
-            WHERE uf.user_id = (SELECT id FROM skate_users WHERE username = ?)
-        `, [currentUser]);
+        //Carga los posts favoritos del usuario
+        const [favorites] = await connection.execute(`SELECT bp.* FROM blog_posts bp JOIN user_favorites uf ON bp.id = uf.post_id WHERE uf.user_id = (SELECT id FROM skate_users WHERE username = ?)`, [currentUser]);
 
         res.render('cuenta', { username: currentUser, currentEmail, favorites });
-    } else {
+    } 
+    //Si el usuario y la ruta no coincide, se lleva al login
+    else {
         res.redirect('/login');
     }
 });
 
 
+//Ruta para cerrar sesión
 router.get('/log_out', (req, res) => {
 
+    //La variable de sesión se vuelve a poner en false
     user_regis = false
 
     res.redirect('/')
 
 })
 
+//Ruta para acceder al reseteo de contraseña
 router.get('/new_pass', (req, res) => {
 
+    //Si el usuario está registrado, se lleva a la ruta donde se resetea la contraseña de ese perfil registrado
     if(user_regis) {
 
         res.redirect('/new_pass/'+currentUser)
 
     }
+    //Si el usuario no está registrado, se prohibe el acceso a esa dirección
     else {
 
         res.sendStatus(403)
@@ -347,18 +363,22 @@ router.get('/new_pass', (req, res) => {
 
 })
 
+//Ruta para acceder al reseteo de contraseña del usuario
 router.get('/new_pass/:username', async(req, res) => {
 
+    //Se coge el usuario de la dirección
     const {username} = req.params
     const connection = await connectBD()
 
     const [usuario] = await connection.execute('SELECT * FROM skate_users WHERE username = ?', [username])
 
+    //Si el usuario está registrado y el usuario registrado es el mismo que el de la ruta, puede acceder
     if(user_regis == true && username == currentUser) {
 
         res.render('new_pass', {username})
 
     }
+    //Si no, se prohibe el acceso a esa dirección
     else {
 
         res.sendStatus(403)
@@ -367,13 +387,15 @@ router.get('/new_pass/:username', async(req, res) => {
 
 })
 
+//Ruta para el reseto de contraseña del usuario actual
 router.post('/new_pass/:username', async (req, res) => {
-    const { username } = req.params;
-    const { pass } = req.body;
+    const { username } = req.params; //Se coge el usuario de la dirección
+    const { pass } = req.body; //Se coge la nueva contraseña introducida
     const connection = await connectBD();
-    const encriptPass = await bcrypt.hash(pass, 10);
-    await connection.execute('UPDATE skate_users SET pass = ? WHERE username = ?', [encriptPass, username]);
+    const encriptPass = await bcrypt.hash(pass, 10); //Se encripta la contraseña en la base de datos
+    await connection.execute('UPDATE skate_users SET pass = ? WHERE username = ?', [encriptPass, username]); //Se actualiza la contraseña del usuario en la base de datos
 
+    //Se envía un correo de que la contraseña del usuario ha sido cambiada
     const mailOptions = {
         from: process.env.EMAIL_USER,
         to: currentEmail,
@@ -392,6 +414,7 @@ router.post('/new_pass/:username', async (req, res) => {
     res.redirect('/cuenta');
 });
 
+//Ruta para el error 404
 router.use((req, res, next) => {
 
     res.status(404).render('404')
